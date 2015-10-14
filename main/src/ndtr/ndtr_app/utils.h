598cabc153683cdddaa4701fabe0ec6a371b37fd
imilos@gmail.com <imilos@gmail.com>
2015-10-14 15:15:08 +0200

    Stigao do Razdvajanja tragova od pozadine

--- main/src/ndtr/ndtr_app/utils.h
+++ main/src/ndtr/ndtr_app/utils.h
@@ -8,12 +8,12 @@ class Utils
 {
 public:
 
-    static std::string StringQ2W(QString& str)
+    static std::string StringQ2W(QString str)
     {
         return str.toStdString();
     }
 
-    static QString StringW2Q(std::string& str)
+    static QString StringW2Q(std::string str)
     {
         return QString(str.c_str());
     }
